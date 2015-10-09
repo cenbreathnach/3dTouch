9ad344a3e681b77d760767efd51c958c31187ac8
Cen Breathnach <cen@donedeal.ie>
2015-10-09 22:50:47 +0100

    Setup project and added 3D touch options to app icon.

---
+++ 3DTouchAPIS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  3DTouchAPIS
+//
+//  Created by Cen Breathnach on 10/9/15.
+//  Copyright © 2015 Cen Breathnach. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
